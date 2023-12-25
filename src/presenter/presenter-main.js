4539ffdaa1485eee52aad3f9c785fa334a06433d
Виктория <nikitina.post@gmail.com>
2023-12-25 21:41:56 +0300

    Закомментирован код показа формы

--- src/presenter/presenter-main.js
+++ src/presenter/presenter-main.js
@@ -3,7 +3,7 @@ import Sorting from '../view/sorting.js';
 import EventsList from '../view/events-list.js';
 import EventsListItem from '../view/events-list-item.js';
 import Event from '../view/event.js';
-import NewPoint from '../view/new-point.js';
+//import NewPoint from '../view/new-point.js';
 
 export default class PresenterMain {
   constructor ({presenterContainer, destinationModel, offersModel, pointsModel}) {
@@ -22,22 +22,22 @@ export default class PresenterMain {
     this.eventsListComponent = new EventsList(); //ul
     this.eventListItemComponent = new EventsListItem(); //li
 
-    let destinationId = this.points[0].destination; //id направления
-    let type = this.points[0].type; //тип направления
-    let destination = this.destinationModel.getById(destinationId);
-    let offers = this.offersModel.getByType(type);
-    this.formComponent = new NewPoint({point: this.points[0], offers: offers, destination: destination});
+    //let destinationId = this.points[0].destination; //id направления
+    //let type = this.points[0].type; //тип направления
+    //let destination = this.destinationModel.getById(destinationId);
+    //let offers = this.offersModel.getByType(type);
+    //this.formComponent = new NewPoint({point: this.points[0], offers: offers, destination: destination});
 
     render(this.sortingComponent, this.presenterContainer); //сортировка
     render(this.eventsListComponent, this.presenterContainer); //список ul
-    render(this.eventListItemComponent, this.eventsListComponent.element); // пункт списка li
-    render(this.formComponent, this.eventListItemComponent.element); //кладем форму в первый li
+    //render(this.eventListItemComponent, this.eventsListComponent.element); // пункт списка li
+    //render(this.formComponent, this.eventListItemComponent.element); //кладем форму в первый li
 
     for (let i = 1; i < this.points.length; i++) {
-      destinationId = this.points[i].destination;
-      destination = this.destinationModel.getById(destinationId);
-      type = this.points[i].type;
-      offers = this.offersModel.getByType(type);
+      const destinationId = this.points[i].destination;
+      const destination = this.destinationModel.getById(destinationId);
+      const type = this.points[i].type;
+      const offers = this.offersModel.getByType(type);
       render(new EventsListItem(), this.eventsListComponent.element); //рендерим li
       render(new Event({point: this.points[i], offers: offers, destination: destination}), this.eventsListComponent.element.lastElementChild); //рендерим point в li
     }

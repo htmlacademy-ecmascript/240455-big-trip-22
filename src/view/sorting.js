cbe7c27d906cf1e33070b63402624d1af3599a5d
Виктория <nikitina.post@gmail.com>
2024-01-24 21:10:19 +0300

    Добавлен checked сортировке по дням

--- src/view/sorting.js
+++ src/view/sorting.js
@@ -7,7 +7,7 @@ function createSorting(sorting) {
     `<form class="trip-events__trip-sort  trip-sort" action="#" method="get">
       ${Object.values(sorting).map((sortName) =>
       `<div class="trip-sort__item  trip-sort__item--${sortName}">
-        <input id="sort-${sortName}" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-${sortName}" ${sortName === 'event' || sortName === 'offers' ? 'disabled' : ''}>
+        <input id="sort-${sortName}" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-${sortName}" ${sortName === 'event' || sortName === 'offers' ? 'disabled' : ''} ${sortName === 'day' ? ' checked' : ''}>
         <label data-sort-type="${sortName}" class="trip-sort__btn" for="sort-${sortName}">${ucFirst(sortName)}</label>
       </div>`).join('')}
     </form>`);

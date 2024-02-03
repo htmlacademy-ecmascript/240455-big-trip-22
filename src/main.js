52fe3539f4657223fcec48369b08050b9752f5ac
Виктория <nikitina.post@gmail.com>
2024-02-03 21:56:06 +0300

    Добавляет модель для фильтра

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import PresenterHeader from './presenter/presenter-header.js';
 import PresenterMain from './presenter/presenter-main.js';
 import MockService from './service/mock-service.js';
-import PointsModel from './model/model.js';
+import PointsModel from './model/point-model.js';
+import FilterModel from './model/filter-model.js';
 
 const mockService = new MockService();
 const pointsModel = new PointsModel(mockService);
+const filterModel = new FilterModel();
 const siteTripEventsContainer = document.querySelector('.trip-events');
 
 const presenterHeader = new PresenterHeader({pointsModel});
